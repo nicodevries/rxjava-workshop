647e00464efcbdcb2394279b87493927cc3fc407
CE84GG <nico.de.vries2@ing.nl>
2019-10-31 16:43:14 +0100

    let testcode provide the Observable, so I can write multiple tests for the same method, to prevent gaming

--- src/main/java/nl/ing/mortgages/rxjava/excercise/ExcerciseWithIntegers.java
+++ src/main/java/nl/ing/mortgages/rxjava/excercise/ExcerciseWithIntegers.java
@@ -1,12 +1,10 @@
 package nl.ing.mortgages.rxjava.excercise;
 
 import io.reactivex.rxjava3.core.Observable;
-import nl.ing.mortgages.rxjava.Observables;
 
 public class ExcerciseWithIntegers {
-    private Observable<Integer> integers = Observables.integers();
 
-    public long countNumberOfEvents() {
-        return integers.count().blockingGet();
+    public long countNumberOfEvents(Observable<Integer> source) {
+        return source.count().blockingGet();
     }
 }
--- src/test/java/nl/ing/mortgages/rxjava/excercise/ExcerciseWithIntegersTest.java
+++ src/test/java/nl/ing/mortgages/rxjava/excercise/ExcerciseWithIntegersTest.java
@@ -1,5 +1,7 @@
 package nl.ing.mortgages.rxjava.excercise;
 
+import io.reactivex.rxjava3.core.Observable;
+import nl.ing.mortgages.rxjava.Observables;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,14 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExcerciseWithIntegersTest {
     private ExcerciseWithIntegers excercise;
+    private Observable<Integer> integers;
 
     @BeforeEach
     void setUp() {
+        integers = Observables.integers();
         excercise = new ExcerciseWithIntegers();
     }
 
     @Test
     void shouldCountHundredEvents() {
-        assertEquals(100, excercise.countNumberOfEvents());
+        assertEquals(100, excercise.countNumberOfEvents(integers));
     }
 }
