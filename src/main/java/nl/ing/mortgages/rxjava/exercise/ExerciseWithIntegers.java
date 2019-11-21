ebae3f8d39e4ddadf9b64c707ac0dddc8e50f80e
Nico de Vries <freezemaster72@hotmail.com>
2019-11-21 13:23:52 +0100

    change order of exercises, add some documentation

--- src/main/java/nl/ing/mortgages/rxjava/exercise/ExerciseWithIntegers.java
+++ src/main/java/nl/ing/mortgages/rxjava/exercise/ExerciseWithIntegers.java
@@ -5,10 +5,6 @@ import io.reactivex.rxjava3.core.Single;
 
 public class ExerciseWithIntegers {
 
-    public long countNumberOfEvents(Observable<Integer> source) {
-        return source.count().blockingGet();
-    }
-
     public Observable<Integer> getEvenNumbers(Observable<Integer> integers) {
         return integers.filter(x -> x % 2 == 0);
     }
@@ -25,5 +21,13 @@ public class ExerciseWithIntegers {
      */
     public Observable<Integer> getSequenceOfCountingUpToEachInteger(Observable<Integer> integers) {
        return integers.flatMap(x -> Observable.range(0, x + 1));
+    }
+
+    /** In the next exercise you need to do something that goes against the principles of Reactive Extensions:
+     * Blocking. In order to count the number of events from a source, you first need to be sure that it has
+     * completed. Usually you only want one of these 'terminal' operators. Don't break the chain!
+     */
+    public long countNumberOfEvents(Observable<Integer> source) {
+        return source.count().blockingGet();
     }
 }
